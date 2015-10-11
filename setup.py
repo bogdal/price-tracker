9e81ff075723762fc5dcd468fdf80df46a5c4d73
Adam Bogdał <adam@bogdal.pl>
2015-10-11 15:17:01 +0200

    Add license

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Adam Bogdal',
     author_email='adam@bogdal.pl',
     url='https://github.com/bogdal/price-tracker',
+    license='BSD',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
